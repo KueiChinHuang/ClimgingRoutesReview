dc705ba02a28a8e09afce4d0f0eeea4c7a2ddfa7
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-08-02 21:12:28 -0400

    fix className

--- client/src/components/reviews/_SearchForm.jsx
+++ client/src/components/reviews/_SearchForm.jsx
@@ -17,14 +17,14 @@ const SearchForm = function ({searchTerms, setSearchTerms}) {
     return (
         <Form inline onSubmit={e => { e.preventDefault(); }}>
             <Form.Group>
-                <InputGroup className="">
+                <InputGroup>
                     <InputGroup.Prepend >
-                    <InputGroup.Text className=""><i class="fa fa-search"></i></InputGroup.Text>
+                    <InputGroup.Text><i className="fa fa-search"></i></InputGroup.Text>
                     </InputGroup.Prepend>
                     <FormControl className="mr-sm-2" placeholder="Search ..." onChange={handleInputChange} value={searchTerms}/>
                 </InputGroup>
             </Form.Group>
-            <Button className="" type="reset" onClick={handleClear}>
+            <Button type="reset" onClick={handleClear}>
                 Clear
             </Button>
         </Form>
