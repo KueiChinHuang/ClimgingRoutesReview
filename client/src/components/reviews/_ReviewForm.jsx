8be05274195425bbc0cf14e997872845a1c08176
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-08-04 13:25:06 -0400

    combine _OptionsForm with _ReviewForm

--- client/src/components/reviews/_ReviewForm.jsx
+++ client/src/components/reviews/_ReviewForm.jsx
@@ -1,8 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Form, Col } from 'react-bootstrap';
-import OptionsForm from './_OptionsForm';
+import Axios from 'axios';
 
 const ReviewForm = function ({ inputs, setInputs }) {
+
+    // Set up options for color, location, and score
+    const [colorOptions, setColorOptions] = useState([]);
+    const [locationOptions, setLocationOptions] = useState([]);
+    const [scoreOptions, setScoreOptions] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            await getColorOptions();
+            await getLocationOptions();
+            await getScoreOptions();
+        })();
+    }, []);
+
+    const getColorOptions = async () => {
+        const colorOptionsResp = await Axios.get('/api/reviews/colorOptions');
+        if (colorOptionsResp.status === 200) setColorOptions(colorOptionsResp.data);
+    };
+
+    const getLocationOptions = async () => {
+        const locationOptionsResp = await Axios.get('/api/reviews/locationOptions');
+        if (locationOptionsResp.status === 200) setLocationOptions(locationOptionsResp.data);
+    };
+
+    const getScoreOptions = async () => {
+        const scoreOptionsResp = await Axios.get('/api/reviews/scoreOptions');
+        if (scoreOptionsResp.status === 200) setScoreOptions(scoreOptionsResp.data);
+    };
+
 
     const handleInputChange = async event => {
         event.persist();
@@ -19,7 +48,53 @@ const ReviewForm = function ({ inputs, setInputs }) {
     return (
         <Fragment>
             <Form.Row className="py-2">
-                <OptionsForm handleInputChange={handleInputChange} inputs={inputs} setInputs={setInputs} />
+                <Col>
+                <Form.Label>Location: </Form.Label>
+                <Form.Control
+                    as="select"
+                    name="location"
+                    onChange={handleInputChange}
+                    value={inputs.location}
+                >
+                    {locationOptions.map((opt, i) => (
+                        <option key={i} value={opt}>{opt}</option>
+                    ))}
+                </Form.Control>
+            </Col>
+            <Col>
+                <Form.Label>Color: </Form.Label>
+                <Form.Control
+                    as="select"
+                    name="color"
+                    onChange={handleInputChange}
+                    value={inputs.color}
+                >
+                    {colorOptions.map((opt, i) => (
+                        <option key={i} value={opt}>{opt}</option>
+                    ))}
+                </Form.Control>
+            </Col>
+            <Col>
+                <Form.Label>Score: </Form.Label>
+                <Form.Control
+                    as="select"
+                    name="score"
+                    onChange={handleInputChange}
+                    value={inputs.score}
+                >
+                    {scoreOptions.map((opt, i) => (
+                        <option key={i} value={opt}>{opt}</option>
+                    ))}
+                </Form.Control>
+            </Col>
+            <Col>
+                <Form.Label>Difficulty: </Form.Label>
+                <Form.Control
+                    name="difficulty"
+                    onChange={handleInputChange}
+                    value={inputs.difficulty}
+                />
+            </Col>
             </Form.Row>
             <Form.Row className="py-2">
                 <Col>
